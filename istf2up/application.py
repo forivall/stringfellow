8039bbbd20159e2008262d5b8af48f12a0dea0fd
Jordan Klassen <forivall@gmail.com>
2012-07-12 23:27:04 -0700

    template mockup working :)

--- istf2up/application.py
+++ istf2up/application.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+#from jinja2 import Environment
 app = Flask(__name__)
 
+#jenv = app.create_jinja_environment()
+
+def yesno_filter(val):
+    return 'yes' if val else 'no'
+app.jinja_env.filters['yesno'] = yesno_filter
+
+def updown_filter(val):
+    return 'up' if val else 'down'
+app.jinja_env.filters['updown'] = updown_filter
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root():
+    return render_template('root.html', isup=True, serverstatuses=[])
 
 if __name__ == '__main__':
     app.run(debug=True)
